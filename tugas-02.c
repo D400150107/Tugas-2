b3408b2d1cfa97b31fa327adce1afc4274f4902e
D400150107 <rakashisui.21@gmail.com>
2015-09-18 16:31:15 +0700

    Create tugas-02.c

---
+++ tugas-02.c
@@ -0,0 +1,18 @@
+Pseudocode
+1. inisialisasi variable a,b,c,D,x1,x2;
+2. D=b^2-4*a*c
+3. if (D=0)
+4. x1=x2=-b/2*a
+5. print (x1=x2)
+6. else
+7. if (D>)
+8. x1=-b+D/2*a
+9. print (x1)
+10. x2=-b-D/2*a
+11. print (x2)
+12. else
+13. x1=-b+(-D)/2*a
+14. print (x1)
+15. x2=-b/2*a-(-D)/2*a
+16. print (x2)
+17. Finish
